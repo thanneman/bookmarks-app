95e30552009817c09eac17d03db17a26ebcd39d3
thanneman <thanneman1@gmail.com>
2020-01-21 12:35:54 -0700

    Newest code

---
+++ build/precache-manifest.a748c9eb3867f8519f129a14db950bcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23ab3a04a1abe40b4a6c",
+    "url": "/static/css/main.80fccc42.chunk.css"
+  },
+  {
+    "revision": "23ab3a04a1abe40b4a6c",
+    "url": "/static/js/main.23ab3a04.chunk.js"
+  },
+  {
+    "revision": "91e13bc2b00622466eb9",
+    "url": "/static/js/1.91e13bc2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ea659cd262a51173582b332845e5cf49",
+    "url": "/index.html"
+  }
+];
